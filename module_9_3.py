7fef3d67d2d77c875b8714060e6b2db4117a3e4c
bochar68 <bochar.dg@gmail.com>
2024-11-26 12:27:56 +0300

    правка комментов

--- module_9_3.py
+++ module_9_3.py
@@ -1,4 +1,5 @@
 # Генераторные сборки
+import sys
 
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
@@ -7,17 +8,17 @@ print('\nРезультат работы генераторной сборки:'
 first_result = (abs(len(f) - len(s)) for f, s in zip(first, second) if len(f) != len(s))
 second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
-print(f'тип объекта: {first_result}') # вывод типа объекта - generator
+print(f'тип объекта: {type(first_result)}') # вывод типа объекта - generator
 print(list(first_result)) # вывод результата работы генераторной сборки
-print(f'тип объекта: {second_result}') # вывод типа объекта - generator
+print(f'тип объекта: {type(second_result)}') # вывод типа объекта - generator
 print(list(second_result)) # вывод результата работы генераторной сборки
 
 
-print('\nРезультат переделки в списковую сборку:')
+print('\nРезультат переделки в списочную сборку:')
 first_result = [abs(len(f) - len(s)) for f, s in zip(first, second) if len(f) != len(s)]
 second_result = [len(first[i]) == len(second[i]) for i in range(len(first))]
 
 print(f'тип объекта: {type(first_result)}') # вывод типа объекта
-print(first_result) # вывод результата работы  сборки
-print(f'тип объекта: {type(second_result)}') # вывод типа объекта - generator
-print(second_result) # вывод результата работы генераторной сборки
+print(first_result) # вывод результата работы сборки
+print(f'тип объекта: {type(second_result)}') # вывод типа объекта
+print(second_result) # вывод результата работы сборки
