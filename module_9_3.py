4c340b3e1b59430426b3ffdf5f0a267d904d8c63
bochar68 <bochar.dg@gmail.com>
2024-11-26 12:12:27 +0300

    понять механизм создания генераторных сборок и использования встроенных функций-генераторов

---
+++ module_9_3.py
@@ -0,0 +1,23 @@
+# Генераторные сборки
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+print('\nРезультат работы генераторной сборки:')
+first_result = (abs(len(f) - len(s)) for f, s in zip(first, second) if len(f) != len(s))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+print(f'тип объекта: {first_result}') # вывод типа объекта - generator
+print(list(first_result)) # вывод результата работы генераторной сборки
+print(f'тип объекта: {second_result}') # вывод типа объекта - generator
+print(list(second_result)) # вывод результата работы генераторной сборки
+
+
+print('\nРезультат переделки в списковую сборку:')
+first_result = [abs(len(f) - len(s)) for f, s in zip(first, second) if len(f) != len(s)]
+second_result = [len(first[i]) == len(second[i]) for i in range(len(first))]
+
+print(f'тип объекта: {type(first_result)}') # вывод типа объекта
+print(first_result) # вывод результата работы  сборки
+print(f'тип объекта: {type(second_result)}') # вывод типа объекта - generator
+print(second_result) # вывод результата работы генераторной сборки
